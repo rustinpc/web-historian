2395e7dc193c4121837c8d91718377dc26052a1a
rustinpc <rustinpc@gmail.com>
2014-11-19 17:35:51 -0800

    Passed test for writing URLs to the sites.txt file.

--- web/request-handler.js
+++ web/request-handler.js
@@ -8,18 +8,37 @@ var fs = require('fs');
 
 var routes = {
                "/": './public/index.html',
+               "/styles.css": './public/styles.css',
+               "/loading.html": './public/loading.html',
                "/www.google.com": '../archives/sites/www.google.com'
              };
 
 exports.handleRequest = function (req, res) {
-  var route = url.parse(req.url).pathname;
-  console.log("route", route);
-  if (routes[route]) {
-    res.writeHead(200, httpHelpers.headers);
-    httpHelpers.serveAssets(res, routes[route], res.end.bind(res));
+  if (req.method === "POST") {
+    var content = "";
+    req.on("data", function (chunk) {
+      content += chunk;
+    });
+
+    req.on("end", function(){
+      console.log("content", content);
+      content = content.slice(4) + "\n";
+      res.writeHead(302);
+      fs.appendFile(archive.paths.list, content, function (err) {
+        if (err) { throw err }
+        res.end("");
+      });
+    });
   } else {
-    res.writeHead(404);
-    res.end("");
+    var route = url.parse(req.url).pathname;
+    console.log("route", route);
+    if (routes[route]) {
+      res.writeHead(200, httpHelpers.headers);
+      httpHelpers.serveAssets(res, routes[route], res.end.bind(res));
+    } else {
+      res.writeHead(404);
+      res.end("");
+    }
   }
   // fs.readFile(path.join(__dirname, './public/index.html'), 'utf8', function(err,data) {
   //                 if (err) { throw err }
