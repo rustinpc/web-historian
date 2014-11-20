1e27917fda13d0fcb069f0126e541defb8fdb9ea
rustinpc <rustinpc@gmail.com>
2014-11-19 16:45:45 -0800

    Passed 2 tests checking routing, can load html files based off of URL.

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -2,6 +2,7 @@ var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 
+
 exports.headers = headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
@@ -10,9 +11,18 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+//asset should be the path to the file
+//callback is what we want to do with the data, e.g. res.end(data)
+
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  console.log("cb ",callback);
+  fs.readFile(path.join(__dirname, asset), 'utf8', function(err,data) {
+    if (err) { throw err }
+    callback(data);
+  });
+
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,32 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var url = require('url');
+var httpHelpers = require('./http-helpers');
+var fs = require('fs');
+
 // require more modules/folders here!
 
+var routes = {
+               "/": './public/index.html',
+               "/www.google.com": '../archives/sites/www.google.com'
+             };
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var route = url.parse(req.url).pathname;
+  console.log("route", route);
+  if (routes[route]) {
+    res.writeHead(200, httpHelpers.headers);
+    httpHelpers.serveAssets(res, routes[route], res.end.bind(res));
+  } else {
+    res.writeHead(404);
+    res.end("");
+  }
+  // fs.readFile(path.join(__dirname, './public/index.html'), 'utf8', function(err,data) {
+  //                 if (err) { throw err }
+  //                 res.end(data);
+  //               });
+
+
+  // res.end(output);
 };
+  // res.end(archive.paths.list);
